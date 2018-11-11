9d40d0e8112c3e9676a0f5f4bbb93c1f7eaa6a7a
Ace Olszowka <aolszowka@gmail.com>
2018-11-11 12:01:08 -0700

    Initial commit, this tool is still very raw, not production ready yet.

---
+++ MsBuildResyncProjectReferenceGuid/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MsBuildResyncProjectReferenceGuid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string targetDirectory = @"S:\TimsSVN\8x\Trunk";
+            ResyncProjectReferenceGuid.Execute(targetDirectory, true);
+        }
+    }
+}
---
+++ MsBuildResyncProjectReferenceGuid/ResyncProjectReferenceGuid.cs
@@ -0,0 +1,101 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ResyncProjectReferenceGuid.cs" company="Ace Olszowka">
+//  Copyright (c) Ace Olszowka 2018. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MsBuildResyncProjectReferenceGuid
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+
+    static class ResyncProjectReferenceGuid
+    {
+        public static bool Execute(string targetDirectory, bool saveModifications)
+        {
+            bool changesMade = false;
+
+            IEnumerable<string> projectsToResync = GetProjectsInDirectory(targetDirectory);
+
+            Parallel.ForEach(projectsToResync, projectToResync =>
+            {
+                if (ResyncProjectReference(projectToResync, saveModifications))
+                {
+                    changesMade = true;
+                }
+            }
+            );
+
+            return changesMade;
+        }
+
+        static bool ResyncProjectReference(string pathToProject, bool saveModifications)
+        {
+            bool changesMade = false;
+
+            XDocument projXml = XDocument.Load(pathToProject);
+
+            IEnumerable<XElement> projectReferences = MSBuildUtilities.GetProjectReferenceNodes(projXml);
+
+            string projectDirectory = PathUtilities.AddTrailingSlash(Path.GetDirectoryName(pathToProject));
+
+            foreach (XElement projectReference in projectReferences)
+            {
+                string existingGuid = MSBuildUtilities.GetProjectReferenceGUID(projectReference, pathToProject);
+                string prRelativePath = MSBuildUtilities.GetProjectReferenceIncludeValue(projectReference, pathToProject);
+                string prActualPath = PathUtilities.ResolveRelativePath(projectDirectory, prRelativePath);
+
+                // If the referenced project doesn't exist throw an exception
+                if (!File.Exists(prActualPath))
+                {
+                    string exception = $"In Project `{pathToProject}` Reference `{existingGuid}` was expected to exist at `{prRelativePath}` (`{prActualPath}`) but did not exist.";
+                    throw new InvalidOperationException(exception);
+                }
+
+                // Else get the Guid from that project
+                string referencedProjectGuid = MSBuildUtilities.GetMSBuildProjectGuid(prActualPath);
+
+                if (!existingGuid.Equals(referencedProjectGuid, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    changesMade = true;
+                    MSBuildUtilities.SetProjectReferenceGUID(projectReference, referencedProjectGuid);
+                }
+            }
+
+            if (changesMade && saveModifications)
+            {
+                try
+                {
+                    projXml.Save(pathToProject);
+                }
+                catch
+                {
+                    System.Threading.Thread.Sleep(30000);
+                    projXml.Save(pathToProject);
+                }
+            }
+
+            return changesMade;
+        }
+
+        /// <summary>
+        /// Gets all Project Files that are understood by this
+        /// tool from the given directory and all subdirectories.
+        /// </summary>
+        /// <param name="targetDirectory">The directory to scan for projects.</param>
+        /// <returns>All projects that this tool supports.</returns>
+        static IEnumerable<string> GetProjectsInDirectory(string targetDirectory)
+        {
+            HashSet<string> supportedFileExtensions = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase) { ".csproj", ".vbproj", ".synproj" };
+
+            return
+                Directory
+                .EnumerateFiles(targetDirectory, "*proj", SearchOption.AllDirectories)
+                .Where(currentFile => supportedFileExtensions.Contains(Path.GetExtension(currentFile)));
+        }
+    }
+}
